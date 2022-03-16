b4f8fc753f1b75cf54efe06f66afbd82197d8d77
ACEGAZ <garylowles@gmail.com>
2022-03-16 21:26:27 +0000

    finished strength skills and adding dex skills

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,8 +104,193 @@ function vasaraResult() {
   document.getElementById("vasara-roll").innerHTML = vasaraDiceRoll()
 }
 
+//adds longsword input and strength value to a 10 sided die roll
+function longswordDiceRoll() {
+  return document.getElementById("longsword").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of longsword + strength + a 10 sided dice roll to a html span
+function longswordResult() {
+  
+  document.getElementById("longsword-roll").innerHTML = longswordDiceRoll()
+}
 
 
+//adds heavy axe input and strength value to a 10 sided die roll
+function heavyAxeDiceRoll() {
+  return document.getElementById("heavy-axe").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of heavy axe + strength + a 10 sided dice roll to a html span
+function heavyAxeResult() {
+  
+  document.getElementById("heavy-axe-roll").innerHTML = heavyAxeDiceRoll()
+}
+
+//adds great bow input and strength value to a 10 sided die roll
+function greatBowDiceRoll() {
+  return document.getElementById("great-bow").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of great bow + strength + a 10 sided dice roll to a html span
+function greatBowResult() {
+  
+  document.getElementById("great-bow-roll").innerHTML = greatBowDiceRoll()
+}
+
+//adds throwing axes input and strength value to a 10 sided die roll
+function throwingAxesDiceRoll() {
+  return document.getElementById("throwing-axes").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of throwing axes + strength + a 10 sided dice roll to a html span
+function throwingAxesResult() {
+  
+  document.getElementById("throwing-axes-roll").innerHTML = throwingAxesDiceRoll()
+}
+
+//adds slugging input and strength value to a 10 sided die roll
+function sluggingDiceRoll() {
+  return document.getElementById("slugging").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of throwing axes + strength + a 10 sided dice roll to a html span
+function sluggingResult() {
+  
+  document.getElementById("slugging-roll").innerHTML = sluggingDiceRoll()
+}
+
+//adds might input and strength value to a 10 sided die roll
+function mightDiceRoll() {
+  return document.getElementById("might").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of might + strength + a 10 sided dice roll to a html span
+function mightResult() {
+  
+  document.getElementById("might-roll").innerHTML = mightDiceRoll()
+}
+
+//adds climbing input and strength value to a 10 sided die roll
+function climbingDiceRoll() {
+  return document.getElementById("climbing").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of climbing + strength + a 10 sided dice roll to a html span
+function climbingResult() {
+  
+  document.getElementById("climbing-roll").innerHTML = climbingDiceRoll()
+}
+
+//adds jumping input and strength value to a 10 sided die roll
+function jumpingDiceRoll() {
+  return document.getElementById("jumping").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of jumping + strength + a 10 sided dice roll to a html span
+function jumpingResult() {
+  
+  document.getElementById("jumping-roll").innerHTML = jumpingDiceRoll()
+}
+
+//adds blocking input and strength value to a 10 sided die roll
+function blockingDiceRoll() {
+  return document.getElementById("blocking").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of blocking + strength + a 10 sided dice roll to a html span
+function blockingResult() {
+  
+  document.getElementById("blocking-roll").innerHTML = blockingDiceRoll()
+}
+
+//adds smithing input and strength value to a 10 sided die roll
+function smithingDiceRoll() {
+  return document.getElementById("smithing").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of smithing + strength + a 10 sided dice roll to a html span
+function smithingResult() {
+  
+  document.getElementById("smithing-roll").innerHTML = smithingDiceRoll()
+}
+
+//adds Intimidation input and strength value to a 10 sided die roll
+function intimidationDiceRoll() {
+  return document.getElementById("intimidation").valueAsNumber + document.getElementById("strength").valueAsNumber + numberGenerator()
+}
+
+// prints results of Intimidation + strength + a 10 sided dice roll to a html span
+function intimidationResult() {
+  
+  document.getElementById("intimidation-roll").innerHTML = intimidationDiceRoll()
+}
+
+//adds daggers input and dexterity value to a 10 sided die roll
+function daggersDiceRoll() {
+  return document.getElementById("daggers").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of daggers + dexterity + a 10 sided dice roll to a html span
+function daggersResult() {
+  
+  document.getElementById("daggers-roll").innerHTML = daggersDiceRoll()
+}
+
+//adds swords input and dexterity value to a 10 sided die roll
+function swordsDiceRoll() {
+  return document.getElementById("swords").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of swords + dexterity + a 10 sided dice roll to a html span
+function swordsResult() {
+  
+  document.getElementById("swords-roll").innerHTML = swordsDiceRoll()
+}
+
+//adds throwing knives input and dexterity value to a 10 sided die roll
+function throwingKnivesDiceRoll() {
+  return document.getElementById("throwing-knives").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of throwing knives + dexterity + a 10 sided dice roll to a html span
+function throwingKnivesResult() {
+  
+  document.getElementById("throwing-knives-roll").innerHTML = throwingKnivesDiceRoll()
+}
+
+//adds spears input and dexterity value to a 10 sided die roll
+function spearsDiceRoll() {
+  return document.getElementById("spears").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of spears + dexterity + a 10 sided dice roll to a html span
+function spearsResult() {
+  
+  document.getElementById("spears-roll").innerHTML = spearsDiceRoll()
+}
+
+//adds war axes input and dexterity value to a 10 sided die roll
+function warAxesDiceRoll() {
+  return document.getElementById("war-axes").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of war axes + dexterity + a 10 sided dice roll to a html span
+function warAxesResult() {
+  
+  document.getElementById("war-axes-roll").innerHTML = warAxesDiceRoll()
+}
+
+//adds short bows axes input and dexterity value to a 10 sided die roll
+function shortBowsDiceRoll() {
+  return document.getElementById("short-bows").valueAsNumber + document.getElementById("dexterity").valueAsNumber + numberGenerator()
+}
+
+// prints results of short bows + dexterity + a 10 sided dice roll to a html span
+function shortBowsResult() {
+  
+  document.getElementById("short-bows-roll").innerHTML = shortBowsDiceRoll()
+}
 
  
  
